6ef62076702039cdbcd71fb2a49e70e70ca20fe4
javimac-dev <javimac.dev@gmail.com>
2023-11-17 16:22:34 -0500

    20231117

--- HumedadBonsai/HumedadBonsai.ino
+++ HumedadBonsai/HumedadBonsai.ino
@@ -1,15 +1,24 @@
+/*
+Control de humedad que hace lecturas cada 5 horas mientras la tierra está sobre el 50% de humedad.  Por debajo del 50% hace lecturas cada segundo.
+Elementos:
+- Arduino uno
+- Sensor de humedad YL-69
+- Módulo receptor YL-38
+- Pantalla LCD 16x2
+*/
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Dirección I2C, 16 columnas y 2 filas
 const int pinSensor = A0;  // Pin analógico donde está conectado el sensor de humedad
-bool retroiluminacionEncendida = true;
 
+bool retroiluminacionEncendida = true;
 int lecturaHumedad; // variable global
 int porcentajeHumedad;
 int delaySeco = 1000;  // lecturas de 1 segundo cuando seco 
-int delayHumedo = 18000000; // lecturas de 5 horas cuando seco
-//int delayHumedo = 1000; // lecturas de 1 seg cuando seco
+int delayHumedo = 18000000; // lecturas de 5 horas cuando húmedo
+//int delayHumedo = 1000; // lecturas de 1 seg cuando húmedo
 
 // seco=1023
 // agua=420
@@ -31,7 +40,6 @@ void setup()
 void loop() 
 {
   lecturaHumedad = analogRead(pinSensor); //lectura del sensor de humedad
-
   while (lecturaHumedad > 721) //entra cuando baja del 50%
   {
     // Enciende o apaga la retroiluminación según el estado actual
@@ -43,28 +51,24 @@ void loop()
     {
       lcd.noBacklight();
     }
-   
     lcd.clear();  // Borra ambas líneas de la pantalla
     lcd.print("AGREGA AGUA-1Lt");
     porcentajeHumedad = static_cast<int>((1 - static_cast<float>(lecturaHumedad - valorMinimo) / (valorMaximo - valorMinimo)) * 100);
     lcd.setCursor(0, 1); // Ubica el cursor en la segunda línea
     lcd.print("Humedad " + String(porcentajeHumedad)+ "%  "+ String(lecturaHumedad));
     lecturaHumedad = analogRead(pinSensor); //lectura del sensor de humedad
-
     // Alterna la retroiluminación en cada ciclo del while
     retroiluminacionEncendida = !retroiluminacionEncendida;   
     delay(delaySeco);
   }
 
   lcd.backlight();//Por si en el último ciclo while queda apagada la pantalla.
-
   /*La siguiente impresión se ubica después del while para que se actualice inmediatamente después de agregar agua al bonsai*/
   lcd.clear();  // Borra ambas líneas de la pantalla
   //  lcd.setCursor(0, 0); // Ubica el cursor en la primera línea
-  lcd.print("  KARATE KIZZ");
+  lcd.print("  KARATE KIZZZ"); // EL QUE ME VENDIÓ EL BONSAI DIJO:  "VEA... ESE ES EL DE KARATE KIZZ"
   porcentajeHumedad = static_cast<int>((1 - static_cast<float>(lecturaHumedad - valorMinimo) / (valorMaximo - valorMinimo)) * 100);
   lcd.setCursor(0, 1); // Ubica el cursor en la segunda línea
   lcd.print("Humedad " + String(porcentajeHumedad)+ "%  "+ String(lecturaHumedad));
-
   delay(delayHumedo); //demora antes de cerrar el loop principal
 }
